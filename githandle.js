7cbf5b1ede3cd7efedaf71317393a9adda5bdd65
Shohei Fujii <fujii.shohei@gmail.com>
2015-09-29 20:51:33 +0900

    s/"/'/g

--- githandle.js
+++ githandle.js
@@ -1,6 +1,6 @@
 'use strict';
 var nodegit = require('nodegit');
-var path = require("path");
+var path = require('path');
 var fse = require('fs-extra');
 var log4js = require('log4js'); 
 var logger = log4js.getLogger('githandle');
@@ -25,14 +25,14 @@ function createGitIssueBranch(gitrepodir) {
                 logger.debug(commit);
                 return commit;
             }, function(reasonForFailure) {
-                logger.debug("failed to get ref of issues, reason:");
+                logger.debug('failed to get ref of issues, reason:');
                 logger.debug(reasonForFailure)
                 return repo.getHeadCommit();
             })
             .then(function(commit) {
                 logger.debug("ok, create a 'issues' branch!");
-                repo.createBranch( "issues", commit, 0, repo.defaultSignature(),
-                        "Created issues branch on HEAD");
+                repo.createBranch( 'issues', commit, 0, repo.defaultSignature(),
+                        'Created issues branch on HEAD');
                 return commit;
             })
             .then(function() {
@@ -60,13 +60,13 @@ function createGitIssueBranch(gitrepodir) {
             .then(function(tree) {
                 // `walk()` returns an event.
                 //var walker = tree.walk();
-                //walker.on("entry", function(entry) {
+                //walker.on('entry', function(entry) {
                     //logger.debug(entry.path());
                     //index.removeByPath(entry.path());
                 //});
                 //walker.start();
                 tree.entries().forEach(function(e){
-                    logger.debug("remove", path.join(repo.workdir(),e.path()));
+                    logger.debug('remove', path.join(repo.workdir(),e.path()));
                     index.removeByPath(e.path());
                     fse.removeSync(path.join(repo.workdir(),e.path()));
                 })
@@ -78,17 +78,17 @@ function createGitIssueBranch(gitrepodir) {
             })
             .then(function(o) {
                 oid = o;
-                return nodegit.Reference.nameToId(repo, "HEAD");
+                return nodegit.Reference.nameToId(repo, 'HEAD');
             })
             .then(function() {
                 var author = repo.defaultSignature();
                 var committer = repo.defaultSignature();
-                //return repo.createCommitOnHead(files, author, committer, "hogehoge");
+                //return repo.createCommitOnHead(files, author, committer, 'hogehoge');
                 logger.debug('commit!!');
-                return repo.createCommit("HEAD", author, committer, "Initialize issues branch", oid, [head])
+                return repo.createCommit('HEAD', author, committer, 'Initialize issues branch', oid, [head])
             })
             .then(function(commitId) {
-                logger.debug("New Commit: ", commitId);
+                logger.debug('New Commit: ', commitId);
             })
             .then(function(){
                 logger.debug('checkout the original branch');
@@ -118,7 +118,7 @@ function addFiles(gitrepodir, files, commitmessage) {
                 return repo.createCommitOnHead(files, author, committer, commitmessage);
             })
             .then(function(commitId) {
-                logger.debug("New Commit: ", commitId);
+                logger.debug('New Commit: ', commitId);
             })
             .then(function(commitId){
                 repo.checkoutBranch(reforig);
